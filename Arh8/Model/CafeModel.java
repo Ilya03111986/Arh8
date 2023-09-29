42b0a4e1b93641d60215717fdeca1e1f906f682a
Ilya Perederey <ilya_soldat@mail.ru>
2023-09-29 20:44:03 +0300

    Arh8

---
+++ Arh8/Model/CafeModel.java
@@ -0,0 +1,44 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Модель данных кафе.
+ */
+public class CafeModel {
+
+  private List<MyTableModel> tables;
+
+  /**
+   * Создает новую модель кафе с заданным именем.
+   * @param name Имя кафе.
+   */
+  public CafeModel(String name) {
+    this.tables = new ArrayList<>();
+  }
+
+//  /**
+//   * Получить имя кафе.
+//   * @return Имя кафе.
+//   */
+//  public String getName() {
+//    return name;
+//  }
+
+  /**
+   * Получить список столов в кафе.
+   * @return Список столов.
+   */
+  public List<MyTableModel> getTables() {
+    return tables;
+  }
+
+  /**
+   * Добавить стол в список столов кафе.
+   * @param table Стол для добавления.
+   */
+  public void addTable(MyTableModel table) {
+    tables.add(table);
+  }
+}
